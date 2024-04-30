a6e45ab3893d2fb38a8295399b1503b210ae38c9
Shalini Mishra <143064135+ShaliniMishra3@users.noreply.github.com>
2024-04-30 22:52:39 +0530

    Add files via upload

---
+++ Level2/Calculator App/s1.js
@@ -0,0 +1,48 @@
+/*let Value_Box = document.getElementById("value");
+
+function value1() {
+  //let Value_Box = document.getElementById("value");
+  let Vb1 = document.getElementById("b1");
+  document.getElementById("value").value = Vb1.value;
+}
+function value2() {
+  let Vb2 = document.getElementById("b2");
+  document.getElementById("value").value = Vb2.value;
+}
+
+function value3() {
+  let Vb3 = document.getElementById("b3");
+  document.getElementById("value").value = Vb3.value;
+}
+
+function value4() {
+  let Vb4 = document.getElementById("b4");
+  document.getElementById("value").value = Vb4.value;
+}
+
+function value5() {
+  let Vb5 = document.getElementById("b5");
+  document.getElementById("value").value = Vb5.value;
+}
+
+function value6() {
+  let Vb6 = document.getElementById("b6");
+  document.getElementById("value").value = Vb6.value;
+}
+
+function value7() {
+  let Vb7 = document.getElementById("b7");
+  document.getElementById("value").value = Vb7.value;
+}
+
+function value8() {
+  let Vb8 = document.getElementById("b8");
+  document.getElementById("value").value = Vb8.value;
+}
+
+function value9() {
+  let Vb9 = document.getElementById("b9");
+  document.getElementById("value").value = Vb9.value;
+}
+*/
+
